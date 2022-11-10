0b2b531ecc07bdfcc92bb1fc451c120720782854
kok-s0s <kok_s0s@163.com>
2022-11-10 10:32:52 +0800

    chore: googletest support

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
-#include <iostream>
+#include <gtest/gtest.h>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
 }
